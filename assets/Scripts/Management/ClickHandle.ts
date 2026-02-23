d72218eca6ed719ab78a691418b3e07cee7437bf
gammapotatodev <egorprytkov.dev@gmail.com>
2026-02-24 01:48:11 +0400

    Добавил обработку Touch_START (для мобилок)

--- assets/Scripts/Management/ClickHandle.ts
+++ assets/Scripts/Management/ClickHandle.ts
@@ -19,31 +19,59 @@ export class ClickHandle extends cc.Component
         this.grid = grid;
 
         // Подписываемся на событие клика мыши на ноде, вызывая метод OnClick при клике
-        this.node.on(cc.Node.EventType.MOUSE_DOWN, this.OnClick, this);
+        if(cc.sys.isMobile)
+            this.node.on(cc.Node.EventType.TOUCH_START, this.OnTouch, this);
+        else
+            this.node.on(cc.Node.EventType.MOUSE_DOWN, this.OnClickMouse, this);
+
     }
 
     // Метод обработки клика. Проверяем, что клик был левой кнопкой мыши, 
     // получаем компонент TileSystem
-    private OnClick(event: cc.Event.EventMouse): void
+    private OnTouch(event: cc.Event.EventTouch): void
     {
-        // Проверяем, что клик был левой кнопкой мыши
+        // // Проверяем, что клик был левой кнопкой мыши
+        // if(event.getButton() !== cc.Event.EventMouse.BUTTON_LEFT)
+        //     return;
+
+        // // Получаем компонент TileSystem для доступа к координатам и индексу группы тайла
+        // const tileComp = this.node.getComponent(TileSystem)!;
+
+        // // Создаём событие TileClickEvent, передавая координаты тайла и индекс группы из компонента TileSystem,
+        // // а также передаём ссылку на ноду
+        // const clickEvent = new TileClickEvent(tileComp.row, tileComp.col, this.node, tileComp.groupIndex);
+
+        // // Запускаем событие через EventManager
+        // EventManager.instance.emit(clickEvent);
+        this.HandleClick();
+
+    }
+
+    private OnClickMouse(event: cc.Event.EventMouse): void
+    {
         if(event.getButton() !== cc.Event.EventMouse.BUTTON_LEFT)
             return;
 
-        // Получаем компонент TileSystem для доступа к координатам и индексу группы тайла
+        this.HandleClick();
+    }
+
+    private HandleClick(): void
+    {
         const tileComp = this.node.getComponent(TileSystem)!;
 
-        // Создаём событие TileClickEvent, передавая координаты тайла и индекс группы из компонента TileSystem,
-        // а также передаём ссылку на ноду
-        const clickEvent = new TileClickEvent(tileComp.row, tileComp.col, this.node, tileComp.groupIndex);
+        const clickEvent = new TileClickEvent(
+            tileComp.row,
+            tileComp.col,
+            this.node,
+            tileComp.groupIndex
+        );
 
-        // Запускаем событие через EventManager
         EventManager.instance.emit(clickEvent);
-
     }
 
     onDestroy() 
     {
-        this.node.off(cc.Node.EventType.MOUSE_DOWN, this.OnClick, this);
+        this.node.off(cc.Node.EventType.MOUSE_DOWN, this.OnClickMouse, this);
+        this.node.off(cc.Node.EventType.TOUCH_START, this.OnTouch, this);
     }
 }
